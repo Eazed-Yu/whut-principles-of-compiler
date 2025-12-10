b149203f110f1c68bbb65c31c5d20e6082f28ad8
Eazed <Eazed_Yu@outlook.com>
2025-12-10 17:11:37 +0800

    词法分析

--- Lexical-nalysis/main.cpp
+++ Lexical-nalysis/main.cpp
@@ -1,18 +1,156 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <map>
 
-// TIP 要<b>Run</b>代码，请按 <shortcut actionId="Run"/> 或点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-int main()
-{
-    // TIP 当文本光标位于 <b>lang</b> 变量名称处时，按 <shortcut actionId="RenameElement"/> 可以查看 CLion 如何帮助您重命名该变量。
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+using namespace std;
+// 关键字映射表
+map<string, string> keywords = {
+    {"const", "CONSTTK"}, {"int", "INTTK"}, {"char", "CHARTK"},
+    {"void", "VOIDTK"}, {"main", "MAINTK"}, {"if", "IFTK"},
+    {"else", "ELSETK"}, {"do", "DOTK"}, {"while", "WHILETK"},
+    {"for", "FORTK"}, {"scanf", "SCANFTK"}, {"printf", "PRINTFTK"},
+    {"return", "RETURNTK"}
+};
 
-    for (int i = 1; i <= 5; i++)
-    {
-        // TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已为您设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点，但您可以随时按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-        std::cout << "i = " << i << std::endl;
+// 判断是否为单纯的单字符符号
+bool isSingleCharSymbol(char c) {
+    return string("+-*/;,()[]{}").find(c) != string::npos;
+}
+
+int main() {
+    ifstream inFile("testfile.txt");
+    ofstream outFile("output.txt");
+
+    if (!inFile.is_open()) {
+        cerr << "无法打开输入文件 testfile.txt" << endl;
+        return 1;
     }
 
+    char ch;
+    while (inFile.get(ch)) {
+        // 1. 跳过空白字符
+        if (isspace(ch)) {
+            continue;
+        }
+
+        // 2. 标识符或保留字
+        if (isalpha(ch) || ch == '_') {
+            string token = "";
+            token += ch;
+            // 继续读取直到不是字母、数字或下划线
+            while (inFile.get(ch)) {
+                if (isalnum(ch) || ch == '_') {
+                    token += ch;
+                } else {
+                    inFile.unget();
+                    break;
+                }
+            }
+
+            // 查表判断是保留字还是标识符
+            if (keywords.count(token)) {
+                outFile << keywords[token] << " " << token << endl;
+            } else {
+                outFile << "IDENFR " << token << endl;
+            }
+        }
+        // 3. 整型常量
+        else if (isdigit(ch)) {
+            string token;
+            token += ch;
+            while (inFile.get(ch)) {
+                if (isdigit(ch)) {
+                    token += ch;
+                } else {
+                    inFile.unget();
+                    break;
+                }
+            }
+            outFile << "INTCON " << token << endl;
+        }
+        // 4. 字符串常量
+        else if (ch == '"') {
+            string token;
+            // 读取直到遇到下一个双引号
+            while (inFile.get(ch)) {
+                if (ch == '"') break;
+                token += ch;
+            }
+            outFile << "STRCON " << token << endl;
+        }
+        // 5. 字符常量
+        else if (ch == '\'') {
+            string token = "";
+            // 读取内容（题目样例似乎不包含转义字符处理，直接取单引号内的内容）
+            while (inFile.get(ch)) {
+                if (ch == '\'') break;
+                token += ch;
+            }
+            outFile << "CHARCON " << token << endl;
+        }
+        // 6. 运算符和界符
+        else {
+            if (ch == '<') {
+                if (inFile.get(ch)) {
+                    if (ch == '=') outFile << "LEQ <=" << endl;
+                    else {
+                        inFile.unget();
+                        outFile << "LSS <" << endl;
+                    }
+                } else outFile << "LSS <" << endl;
+            }
+            else if (ch == '>') {
+                if (inFile.get(ch)) {
+                    if (ch == '=') outFile << "GEQ >=" << endl;
+                    else {
+                        inFile.unget();
+                        outFile << "GRE >" << endl;
+                    }
+                } else outFile << "GRE >" << endl;
+            }
+            else if (ch == '=') {
+                if (inFile.get(ch)) {
+                    if (ch == '=') outFile << "EQL ==" << endl;
+                    else {
+                        inFile.unget();
+                        outFile << "ASSIGN =" << endl;
+                    }
+                } else outFile << "ASSIGN =" << endl;
+            }
+            else if (ch == '!') {
+                if (inFile.get(ch)) {
+                    if (ch == '=') outFile << "NEQ !=" << endl;
+                    else {
+                        inFile.unget();
+                    }
+                }
+            }
+            else {
+                // 单字符符号处理
+                switch (ch) {
+                    case '+': outFile << "PLUS +" << endl; break;
+                    case '-': outFile << "MINU -" << endl; break;
+                    case '*': outFile << "MULT *" << endl; break;
+                    case '/': outFile << "DIV /" << endl; break;
+                    case ';': outFile << "SEMICN ;" << endl; break;
+                    case ',': outFile << "COMMA ," << endl; break;
+                    case '(': outFile << "LPARENT (" << endl; break;
+                    case ')': outFile << "RPARENT )" << endl; break;
+                    case '[': outFile << "LBRACK [" << endl; break;
+                    case ']': outFile << "RBRACK ]" << endl; break;
+                    case '{': outFile << "LBRACE {" << endl; break;
+                    case '}': outFile << "RBRACE }" << endl; break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    inFile.close();
+    outFile.close();
     return 0;
-    // TIP 请访问 <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a> 查看 CLion 帮助。此外，您还可以从主菜单中选择“帮助 | 学习 IDE 功能”，尝试 CLion 的交互式课次。
 }
+
